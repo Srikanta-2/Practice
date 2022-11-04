d58ec046bf01fbe03f6b408badd0278a2955ba1a
srikanta-2 <srikantadash523@gmail.com>
2022-11-04 15:58:37 +0530

    hii

--- CucumberPro/src/test/java/TestRunners/RunnerClass.java
+++ CucumberPro/src/test/java/TestRunners/RunnerClass.java
@@ -5,6 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/java/features", 
 				glue="step_definations",
+			
 				monochrome= true,
 				plugin={"html:target/cucumber-reports"}
 				)
